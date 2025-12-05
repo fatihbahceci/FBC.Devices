9aff094a0d9c9d4c89c89493e39703da305e1156
Fatih Bahceci <fatihbahceci@gmail.com>
2025-12-06 01:16:19 +0300

    cp

--- FBC.Devices.API/MediatR/IRequest.cs
+++ FBC.Devices.API/MediatR/IRequest.cs
@@ -1,162 +1,160 @@
 ﻿using Microsoft.Extensions.DependencyInjection.Extensions;
+using System.Collections.Concurrent;
+using System.Linq.Expressions;
 using System.Reflection;
 
 namespace FBC.Devices.API.MediatR;
 
-public interface IRequest<out TResponse> : IMessage
-{
-}
-public interface IRequest : IMessage
-{
-}
-public interface IMessage
-{
-}
-public interface IRequestHandler<in TRequest, TResponse> 
+public interface IRequestBase;
+public interface IRequest<out TResponse> : IRequestBase;
+public interface IRequest : IRequestBase;
+
+public interface IRequestHandler<in TRequest, TResponse>
     where TRequest : IRequest<TResponse>
 {
-    TResponse Handle(TRequest request);
+    Task<TResponse> Handle(TRequest request, CancellationToken token = default);
 }
-public interface IRequestHandler<in TRequest> 
+public interface IRequestHandler<in TRequest>
     where TRequest : IRequest
 {
-    void Handle(TRequest request);
+    Task Handle(TRequest request, CancellationToken token = default);
 }
-public static class RequestHandlerExtensions
+
+public delegate Task<TResponse> RequestHandlerDelegate<TResponse>();
+
+public interface IMediator
 {
-    public static MethodInfo GetHandleMethodInfo<TRequest, TResponse>(this IRequestHandler<TRequest, TResponse> handler)
-        where TRequest : IRequest<TResponse>
+    Task<TResponse> Send<TResponse>(IRequest<TResponse> request, CancellationToken cancellationToken = default);
+    Task Send(IRequest request, CancellationToken cancellationToken = default);
+}
+
+public class Mediator : IMediator
+{
+    // small marker to represent void in generic pipeline
+    private class VoidMarker { public static readonly VoidMarker Instance = new(); }
+
+    private readonly IServiceProvider _serviceProvider;
+    private readonly ConcurrentDictionary<string, object> _handlerCache = new();
+    public Mediator(IServiceProvider serviceProvider)
     {
-        return handler.GetType().GetMethod("Handle", new Type[] { typeof(TRequest) })!;
+        _serviceProvider = serviceProvider;
     }
-    public static MethodInfo GetHandleMethodInfo<TRequest>(this IRequestHandler<TRequest> handler)
-        where TRequest : IRequest
+    //public async Task<TResponse> Send<TResponse>(IRequest<TResponse> request, CancellationToken cancellationToken = default)
+    //{
+    //    var handlerType = typeof(IRequestHandler<,>).MakeGenericType(request.GetType(), typeof(TResponse));
+    //    dynamic handler = _serviceProvider.GetRequiredService(handlerType);
+    //    return await handler.Handle((dynamic)request, cancellationToken);
+    //}
+    //public async Task Send(IRequest request, CancellationToken cancellationToken = default)
+    //{
+    //    var handlerType = typeof(IRequestHandler<>).MakeGenericType(request.GetType());
+    //    dynamic handler = _serviceProvider.GetRequiredService(handlerType);
+    //    await handler.Handle((dynamic)request, cancellationToken);
+    //}
+
+    public Task<TResponse> Send<TResponse>(IRequest<TResponse> request, CancellationToken cancellationToken = default)
     {
-        return handler.GetType().GetMethod("Handle", new Type[] { typeof(TRequest) })!;
+        if (request == null) throw new ArgumentNullException(nameof(request));
+
+
+        var key = GetCacheKey(request.GetType(), typeof(TResponse));
+        var invoker = (Func<IServiceProvider, object, CancellationToken, Task<TResponse>>)_handlerCache.GetOrAdd(key, _ => BuildRequestInvoker<TResponse>(request.GetType()));
+        return invoker(_serviceProvider, request, cancellationToken);
+    }
+
+
+    public Task Send(IRequest request, CancellationToken cancellationToken = default)
+    {
+        if (request == null) throw new ArgumentNullException(nameof(request));
+
+
+        var key = GetCacheKey(request.GetType(), typeof(VoidMarker));
+        var invoker = (Func<IServiceProvider, object, CancellationToken, Task>)_handlerCache.GetOrAdd(key, _ => BuildVoidRequestInvoker(request.GetType()));
+        return invoker(_serviceProvider, request, cancellationToken);
+    }
+
+    private static string GetCacheKey(Type requestType, Type responseType) => requestType.FullName + "=>" + responseType.FullName;
+
+    private static Func<IServiceProvider, object, CancellationToken, Task<TResponse>> BuildRequestInvoker<TResponse>(Type requestType)
+    {
+        var handlerType = typeof(IRequestHandler<,>).MakeGenericType(requestType, typeof(TResponse));
+        var serviceProviderParam = Expression.Parameter(typeof(IServiceProvider), "serviceProvider");
+        var requestParam = Expression.Parameter(typeof(object), "request");
+        var cancellationTokenParam = Expression.Parameter(typeof(CancellationToken), "cancellationToken");
+        var getRequiredServiceMethod = typeof(ServiceProviderServiceExtensions)
+            .GetMethod(nameof(ServiceProviderServiceExtensions.GetRequiredService), BindingFlags.Static | BindingFlags.Public)
+            .MakeGenericMethod(handlerType);
+
+        var getServiceCall = Expression.Call(
+            null, // static method
+            getRequiredServiceMethod,
+            serviceProviderParam);
+        var handlerVar = Expression.Variable(handlerType, "handler");
+        var assignHandler = Expression.Assign(handlerVar, getServiceCall);
+        var handleMethod = handlerType.GetMethod("Handle");
+        var callHandle = Expression.Call(
+            handlerVar,
+            handleMethod,
+            Expression.Convert(requestParam, requestType),
+            cancellationTokenParam);
+        var lambda = Expression.Lambda<Func<IServiceProvider, object, CancellationToken, Task<TResponse>>>(
+            Expression.Block(
+                new[] { handlerVar },
+                assignHandler,
+                callHandle),
+            serviceProviderParam,
+            requestParam,
+            cancellationTokenParam);
+        return lambda.Compile();
+    }
+    private static Func<IServiceProvider, object, CancellationToken, Task> BuildVoidRequestInvoker(Type requestType)
+    {
+        var handlerType = typeof(IRequestHandler<>).MakeGenericType(requestType);
+        var serviceProviderParam = Expression.Parameter(typeof(IServiceProvider), "serviceProvider");
+        var requestParam = Expression.Parameter(typeof(object), "request");
+        var cancellationTokenParam = Expression.Parameter(typeof(CancellationToken), "cancellationToken");
+
+        var getRequiredServiceMethod = typeof(ServiceProviderServiceExtensions)
+            .GetMethod(nameof(ServiceProviderServiceExtensions.GetRequiredService),BindingFlags.Static | BindingFlags.Public)
+            .MakeGenericMethod(handlerType);
+
+        var getServiceCall = Expression.Call(
+            null, // static method
+            getRequiredServiceMethod,
+            serviceProviderParam);
+
+        var handlerVar = Expression.Variable(handlerType, "handler");
+        var assignHandler = Expression.Assign(handlerVar, getServiceCall);
+        var handleMethod = handlerType.GetMethod("Handle");
+        var callHandle = Expression.Call(
+            handlerVar,
+            handleMethod,
+            Expression.Convert(requestParam, requestType),
+            cancellationTokenParam);
+        var lambda = Expression.Lambda<Func<IServiceProvider, object, CancellationToken, Task>>(
+            Expression.Block(
+                new[] { handlerVar },
+                assignHandler,
+                callHandle),
+            serviceProviderParam,
+            requestParam,
+            cancellationTokenParam);
+        return lambda.Compile();
     }
 }
 
-public static class RequestExtensions
-{
-    public static Type GetResponseType<TRequest, TResponse>(this TRequest request)
-        where TRequest : IRequest<TResponse>
-    {
-        return typeof(TResponse);
-    }
-    public static Type GetResponseType<TRequest>(this TRequest request)
-        where TRequest : IRequest
-    {
-        return typeof(void);
-    }
-}
-
-public static class RequestTypeExtensions
-{
-    public static bool IsRequestOfType<TResponse>(this Type requestType)
-    {
-        if (!typeof(IRequest).IsAssignableFrom(requestType))
-            return false;
-        if (requestType.IsGenericType && requestType.GetGenericTypeDefinition() == typeof(IRequest<>))
-        {
-            var genericArgs = requestType.GetGenericArguments();
-            return genericArgs.Length == 1 && genericArgs[0] == typeof(TResponse);
-        }
-        foreach (var iface in requestType.GetInterfaces())
-        {
-            if (iface.IsGenericType && iface.GetGenericTypeDefinition() == typeof(IRequest<>))
-            {
-                var genericArgs = iface.GetGenericArguments();
-                if (genericArgs.Length == 1 && genericArgs[0] == typeof(TResponse))
-                    return true;
-            }
-        }
-        return false;
-    }
-}
-
-public static class RequestTypeResponseExtensions
-{
-    public static Type GetResponseType(this Type requestType)
-    {
-        if (!typeof(IRequest).IsAssignableFrom(requestType))
-            throw new ArgumentException("Type must implement IRequest interface.", nameof(requestType));
-        if (requestType.IsGenericType && requestType.GetGenericTypeDefinition() == typeof(IRequest<>))
-        {
-            return requestType.GetGenericArguments()[0];
-        }
-        foreach (var iface in requestType.GetInterfaces())
-        {
-            if (iface.IsGenericType && iface.GetGenericTypeDefinition() == typeof(IRequest<>))
-            {
-                return iface.GetGenericArguments()[0];
-            }
-        }
-        return typeof(void);
-    }
-}
-
-public static class RequestHandlerTypeExtensions
-{
-    public static Type GetRequestType<TRequest, TResponse>(this IRequestHandler<TRequest, TResponse> handler)
-        where TRequest : IRequest<TResponse>
-    {
-        return typeof(TRequest);
-    }
-    public static Type GetRequestType<TRequest>(this IRequestHandler<TRequest> handler)
-        where TRequest : IRequest
-    {
-        return typeof(TRequest);
-    }
-}
 
 public interface IEndpoint
 {
     void AddRoutes(IEndpointRouteBuilder app);
 }
 
-public interface IMediator
-{
-    TResponse Send<TResponse>(IRequest<TResponse> request);
-    void Send(IRequest request);
-}
-public class Mediator : IMediator
-{
-    private readonly IServiceProvider _serviceProvider;
-    public Mediator(IServiceProvider serviceProvider)
-    {
-        _serviceProvider = serviceProvider;
-    }
-    public TResponse Send<TResponse>(IRequest<TResponse> request)
-    {
-        var handlerType = typeof(IRequestHandler<,>).MakeGenericType(request.GetType(), typeof(TResponse));
-        dynamic handler = _serviceProvider.GetService(handlerType);
-        if (handler == null)
-            throw new InvalidOperationException($"No handler found for request type {request.GetType().FullName}");
-        return handler.Handle((dynamic)request);
-    }
-    public void Send(IRequest request)
-    {
-        var handlerType = typeof(IRequestHandler<>).MakeGenericType(request.GetType());
-        dynamic handler = _serviceProvider.GetService(handlerType);
-        if (handler == null)
-            throw new InvalidOperationException($"No handler found for request type {request.GetType().FullName}");
-        handler.Handle((dynamic)request);
-    }
-}
-
 public static class MediatorExtensions
 {
-    public static TResponse SendRequest<TResponse>(this IMediator mediator, IRequest<TResponse> request)
-    {
-        return mediator.Send(request);
-    }
-    public static void SendRequest(this IMediator mediator, IRequest request)
-    {
-        mediator.Send(request);
-    }
     public static IServiceCollection AddMediator(this IServiceCollection services, params Assembly[] assemblies)
     {
-        services.AddSingleton<IMediator, Mediator>();
+        //services.AddSingleton<IMediator, Mediator>();
+        services.AddScoped<IMediator, Mediator>();
         var allAssemblies = assemblies.Length > 0 ? assemblies : AppDomain.CurrentDomain.GetAssemblies();
         var handlerTypes = allAssemblies.SelectMany(a => a.GetTypes())
             .Where(t => !t.IsAbstract && !t.IsInterface)
@@ -167,43 +165,36 @@ public static class MediatorExtensions
                 .Select(i => new { HandlerType = t, InterfaceType = i }));
         foreach (var handler in handlerTypes)
         {
-            services.AddTransient(handler.InterfaceType, handler.HandlerType);
+            //Tüm örneklerde transient yazılmış. Ben neden scoped yaptım? 
+            //services.AddTransient(handler.InterfaceType, handler.HandlerType);
+            services.AddScoped(handler.InterfaceType, handler.HandlerType);
+        }
+        // Register IEndpoint implementations
+        //var serviceDescriptors = allAssemblies.SelectMany(assembly => assembly.DefinedTypes.Where(type => type is { IsAbstract: false, IsInterface: false } && type.IsAssignableTo(typeof(IEndpoint))).Select(type => ServiceDescriptor.Transient(typeof(IEndpoint), type)).ToArray());
+        //services.TryAddEnumerable(serviceDescriptors);
+
+        var endpointTypes = allAssemblies
+            .SelectMany(a => a.DefinedTypes)
+            .Where(t => typeof(IEndpoint).IsAssignableFrom(t) && !t.IsAbstract && !t.IsInterface);
+
+        foreach (var endpointType in endpointTypes)
+        {
+            services.AddTransient(endpointType);                    // Kendi tipiyle (opsiyonel ama iyi)
+            services.AddTransient(typeof(IEndpoint), endpointType); // IEnumerable<IEndpoint> için
         }
 
-        var serviceDescriptors = allAssemblies.SelectMany(assembly => assembly.DefinedTypes.Where(type => type is { IsAbstract: false, IsInterface: false } && type.IsAssignableTo(typeof(IEndpoint))).Select(type => ServiceDescriptor.Transient(typeof(IEndpoint), type)).ToArray());
-        services.TryAddEnumerable(serviceDescriptors);
-        //services.AddApiVersion();
+
         return services;
     }
-    //public static IServiceCollection AddApiVersion(this IServiceCollection services)
-    //{
-    //    var builder = services.AddApiVersioning(options =>
-    //    {
-    //        options.DefaultApiVersion = new ApiVersion(1, 0);
-    //        options.ReportApiVersions = true;
-    //        options.AssumeDefaultVersionWhenUnspecified = true;
-    //        options.ApiVersionReader = new UrlSegmentApiVersionReader();
-    //    }).AddApiExplorer(options =>
-    //    {
-    //        options.GroupNameFormat = "'v'VVV";
-    //        options.SubstituteApiVersionInUrl = true;
-    //    });
 
-    //    return services;
-    //}
-
-    public static IApplicationBuilder MapEndpoints(this WebApplication app)
+    public static IApplicationBuilder UseMediatorEndpoints(this IApplicationBuilder app)
     {
-        ApiVersionSet apiVersionSet = app.NewApiVersionSet().HasApiVersion(new ApiVersion(1.0)).HasApiVersion(new ApiVersion(2.0))
-            .ReportApiVersions()
-            .Build();
-        RouteGroupBuilder app2 = app.MapGroup("api/v{v:apiVersion}").WithApiVersionSet(apiVersionSet);
-        IEnumerable<IEndpoint> requiredService = app.Services.GetRequiredService<IEnumerable<IEndpoint>>();
-        foreach (IEndpoint item in requiredService)
+        var endpoints = app.ApplicationServices.GetServices<IEndpoint>();
+        var routeBuilder = app.ApplicationServices.GetRequiredService<IEndpointRouteBuilder>();
+        foreach (var endpoint in endpoints)
         {
-            item.AddRoutes(app2);
+            endpoint.AddRoutes(routeBuilder);
         }
-
         return app;
     }
 }
